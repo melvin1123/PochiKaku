af4fcf2402d37ec02acfffae81ce3d9c9b3dae0c
ztargazer57 <johnmhellsinagpulo@gmail.com>
2026-04-15 14:33:28 +0800

    Update page.tsx

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -91,7 +91,7 @@ export default function EventsPage() {
 
   return (
     <MainLayout>
-      <div className="flex min-h-full flex-col">
+      <div className="flex min-h-fit border-2 flex-col">
         <EventsHeader onCreate={() => setIsEventModalOpen(true)} />
 
         <EventCreationModal
